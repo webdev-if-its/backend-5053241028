6f34bb56d446f877834336f2ccfd3f5df82d270b
ravennn21 <vian21okta@gmail.com>
2026-09-01 19:41:17 +0700

    pertemuan-01: level 5 selesai

--- pertemuan-01/main.go
+++ pertemuan-01/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"os"
+	"runtime"
 )
 
 // TODO(Level 4): ganti dengan NRP kalian sendiri, contoh: "5025201012"
@@ -26,7 +27,10 @@ func Sapa(nama string) string {
 // TODO(Level 5): gabungkan Nama, NRP, dan hasil runtime.Version() jadi satu
 // string siap cetak (lihat contoh format di SOAL.md).
 func CetakInfo(nama string) string {
-	return "TODO"
+
+	return "nama: " + nama + "\n" +
+		"nrp: " + NRP + "\n" +
+		"go version: " + runtime.Version()
 }
 
 func main() {
