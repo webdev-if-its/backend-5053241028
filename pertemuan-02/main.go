b5dd9a767572cf5918b867d7fb3168d531f5a917
ravennn21 <vian21okta@gmail.com>
2026-09-08 17:11:43 +0700

    pertemuan-01: level 3 selesai

--- pertemuan-02/main.go
+++ pertemuan-02/main.go
@@ -23,7 +23,7 @@ func HitungTotalPesanan(qty []int, hargaSatuan []float64) float64 {
 }
 
 func TerapkanPajak(total float64, tarifPajak float64) float64 {
-	panic("belum diimplementasikan")
+	return (total + (tarifPajak * total))
 }
 
 func HitungDiskon(total float64) float64 {
